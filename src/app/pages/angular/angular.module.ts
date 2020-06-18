737688000d37b6a8a2240666a6eddcb8bf224ab4
Leandro <leandrolcosta213@gmail.com>
2020-06-18 17:22:23 -0300

    new: angular component

---
+++ src/app/pages/angular/angular.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularComponent } from './angular.component';
+
+
+
+@NgModule({
+  declarations: [AngularComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AngularModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {HomeModule} from './home/home.module';
 import {SobreMimModule} from './sobre-mim/sobre-mim.module';
 import {JavaModule} from './java/java.module';
+import {AngularModule} from './angular/angular.module';
 
 @NgModule({
   declarations: [],
@@ -10,7 +11,8 @@ import {JavaModule} from './java/java.module';
     CommonModule,
     HomeModule,
     SobreMimModule,
-    JavaModule
+    JavaModule,
+    AngularModule
 
   ]
 })
