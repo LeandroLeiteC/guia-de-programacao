b5f48a81dd494a4873249009d1d93d18bf79a520
Leandro <leandrolcosta213@gmail.com>
2020-06-18 17:23:28 -0300

    new: git-github component

---
+++ src/app/pages/git-github/git-github.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-github',
+  templateUrl: './git-github.component.html',
+  styleUrls: ['./git-github.component.css']
+})
+export class GitGithubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/git-github/git-github.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GitGithubComponent } from './git-github.component';
+
+
+
+@NgModule({
+  declarations: [GitGithubComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GitGithubModule { }
