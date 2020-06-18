db41e48c4627c0c90d6d2a81a10ac6557abff77b
Leandro <leandrolcosta213@gmail.com>
2020-06-18 14:07:53 -0300

    new: criacao sobre-mim component

---
+++ src/app/pages/sobre-mim/sobre-mim.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SobreMimComponent } from './sobre-mim.component';
+
+
+
+@NgModule({
+  declarations: [SobreMimComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SobreMimModule { }
