3a471445626ec8b18236f57f72b32ed2642c371b
Leandro <leandrolcosta213@gmail.com>
2020-06-18 16:19:53 -0300

    edit: layout

--- src/app/pages/sobre-mim/sobre-mim.module.ts
+++ src/app/pages/sobre-mim/sobre-mim.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SobreMimComponent } from './sobre-mim.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [SobreMimComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ]
 })
 export class SobreMimModule { }
