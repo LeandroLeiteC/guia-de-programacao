a8fd5e42317c04025564edc79c397b1a437f5303
Leandro <leandrolcosta213@gmail.com>
2020-06-18 18:01:12 -0300

    new: shared card

---
+++ src/app/shared/card-guias/card-guias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-guias',
+  templateUrl: './card-guias.component.html',
+  styleUrls: ['./card-guias.component.css']
+})
+export class CardGuiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/card-guias/card-guias.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardGuiasComponent } from './card-guias.component';
+
+
+
+@NgModule({
+  declarations: [CardGuiasComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [CardGuiasComponent]
+})
+export class CardGuiasModule { }
