c78385174974773ed21eab6b0618901c8f576b7f
Leandro <leandrolcosta213@gmail.com>
2020-06-18 21:58:39 -0300

    edit: página angular e alinhamento responsivo

--- src/app/pages/angular/angular.component.ts
+++ src/app/pages/angular/angular.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Guia} from '../../shared/models/guia.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-angular',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngularComponent implements OnInit {
 
-  constructor() { }
+  card = false;
+  guias: Guia[] = [];
+  private _urlJson: string = 'assets/db/angular.json';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<Guia[]>(this._urlJson)
+      .subscribe( response => {
+        this.guias = response;
+        this.modoExibicao();
+      });
+  }
+
+  modoExibicao() {
+    this.card = !this.card;
   }
 
 }
--- src/app/pages/angular/angular.module.ts
+++ src/app/pages/angular/angular.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularComponent } from './angular.component';
+import {CardGuiasModule} from '../../shared/card-guias/card-guias.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [AngularComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardGuiasModule,
+    NgbModule
   ]
 })
 export class AngularModule { }
