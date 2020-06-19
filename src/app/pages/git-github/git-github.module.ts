5756893460c0ad932d871653bd1669c5850db00d
Leandro <leandrolcosta213@gmail.com>
2020-06-18 22:03:32 -0300

    edit: página git & github

--- src/app/pages/git-github/git-github.module.ts
+++ src/app/pages/git-github/git-github.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GitGithubComponent } from './git-github.component';
+import {CardGuiasModule} from '../../shared/card-guias/card-guias.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [GitGithubComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardGuiasModule,
+    NgbModule
   ]
 })
 export class GitGithubModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import {HomeModule} from './home/home.module';
 import {SobreMimModule} from './sobre-mim/sobre-mim.module';
 import {JavaModule} from './java/java.module';
 import {AngularModule} from './angular/angular.module';
+import {GitGithubModule} from './git-github/git-github.module';
 
 @NgModule({
   declarations: [],
@@ -12,7 +13,8 @@ import {AngularModule} from './angular/angular.module';
     HomeModule,
     SobreMimModule,
     JavaModule,
-    AngularModule
+    AngularModule,
+    GitGithubModule
 
   ]
 })
